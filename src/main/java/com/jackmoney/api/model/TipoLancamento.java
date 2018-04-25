75155b176aec708036192fdcccdd44fcfeaea8fc
Jack Oliveira <jackolvr@users.noreply.github.com>
2018-04-25 18:04:25 -0300

    add banco e model lancamentos

---
+++ src/main/java/com/jackmoney/api/model/TipoLancamento.java
@@ -0,0 +1,7 @@
+package com.jackmoney.api.model;
+
+public enum TipoLancamento {
+
+	RECEITA,
+	DESPESA
+}
--- src/main/java/com/jackmoney/api/resource/PessoaResource.java
+++ src/main/java/com/jackmoney/api/resource/PessoaResource.java
@@ -68,4 +68,10 @@ public class PessoaResource {
 		return ResponseEntity.ok(pessoaSalva);
 		
 	}
+	
+	@PutMapping("/{codigo}/ativo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizarPropriedadeAtivo(@PathVariable long codigo, @RequestBody Boolean ativo) {
+		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+	}
 }
